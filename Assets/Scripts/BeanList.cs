3f1e67cdf09ed56974602d7fe85f55a7efcd64db
Chesyon <55322011+Chesyon@users.noreply.github.com>
2023-04-26 08:04:53 -0400

    Remove unused namespaces
    Removed things such as System.Collections from scripts that don't use them, should slightly help performance.

--- Assets/Scripts/BeanList.cs
+++ Assets/Scripts/BeanList.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
 public class BeanList : MonoBehaviour
 {
     public RectTransform thisRect;
